2c8fa94e0f8c291ca0f9c6d7f7b593464c38fb3a
Aaron <aaron.tan1@gmail.com>
2025-05-03 21:15:55 +0800

    refactor: move emission factors to constants and update calculation logic

--- app/constants.py
+++ app/constants.py
@@ -7,3 +7,58 @@ nav_items = [
     {"name": "Share", "endpoint": "share", "icon": "share.svg"},
     {"name": "Facts", "endpoint": "facts", "icon": "facts.svg"},
 ]
+
+# Emission factors (in kg CO2e)
+EMISSION_FACTORS = {
+    'vehicle': {
+        'gasoline': 2.34,  # kg CO₂e per litre
+        'diesel': 2.69,    # kg CO₂e per litre
+        'electric': 0.68   # kg CO₂e per kWh
+    },
+    'public_transit': {
+        'simple': 0.036661,    
+        'bus': 0.064622,      
+        'transit_rail': 0.025476, 
+        'commuter_rail': 0.039768, 
+        'intercity_rail': 0.017398 
+    },
+    'air_travel': {
+        'simple': 0.087489,   
+        'short': 0.2,          
+        'medium': 0.15,        
+        'long': 0.12,          
+        'extended': 0.11       
+    },
+    'home': {
+        'electricity_kwh': 0.68,      
+        'electricity_dollar': 0.33,
+        'natural_gas_therm': 0.181, 
+        'natural_gas_m3': 0.038,     
+        'natural_gas_dollar': 4.08, 
+        'heating_oil_litre': 0.671,
+        'heating_oil_dollar': 1.46, 
+        'water_usage': 0.00046,      
+        'construction': 0.42         
+    },
+    'food': {
+        'meat_fish_eggs': 4.05,       
+        'grains_baked_goods': 0.23,   
+        'dairy': 0.95,                
+        'fruits_vegetables': 0.35,    
+        'snacks_drinks': 2.0         
+    },
+    'shopping': {
+        'goods_base': 1311,          
+        'services_base': 2413,       
+        'furniture_appliances': 0.5, 
+        'clothing': 0.6,             
+        'entertainment': 0.4,        
+        'office_supplies': 0.7,      
+        'personal_care': 0.5,        
+        'services_food': 0.3,        
+        'education': 0.2,            
+        'communication': 0.25,       
+        'loan': 0.15,                
+        'transport': 0.35            
+    }
+}
--- app/processing_layer.py
+++ app/processing_layer.py
@@ -1,59 +1,5 @@
 from app.models import CarbonFootprint, Travel, Vehicle, Home, Food, Shopping, Emissions
-
-# Emission factors (in kg CO2e)
-EMISSION_FACTORS = {
-    'vehicle': {
-        'gasoline': 2.34,  # kg CO₂e per litre
-        'diesel': 2.69,    # kg CO₂e per litre
-        'electric': 0   # kg CO₂e per kWh
-    },
-    'public_transit': {
-        'simple': 0.036661,    
-        'bus': 0.064622,      
-        'transit_rail': 0.025476, 
-        'commuter_rail': 0.039768, 
-        'intercity_rail': 0.017398 
-    },
-    'air_travel': {
-        'simple': 0.087489,   
-        'short': 0.2,          
-        'medium': 0.15,        
-        'long': 0.12,          
-        'extended': 0.11       
-    },
-    'home': {
-        'electricity_kwh': 0.4,      
-        'electricity_dollar': 0.008,
-        'natural_gas_therm': 0.181, 
-        'natural_gas_m3': 0.038,     
-        'natural_gas_dollar': 0.006,
-        'heating_oil_litre': 0.00268,
-        'heating_oil_dollar': 0.001, 
-        'water_usage': 0.00113,      
-        'construction': 0.05         
-    },
-    'food': {
-        'meat_fish_eggs': 4.8,       
-        'grains_baked_goods': 1.5,   
-        'dairy': 3.2,                
-        'fruits_vegetables': 0.9,    
-        'snacks_drinks': 2.0         
-    },
-    'shopping': {
-        'goods_base': 1311,          
-        'services_base': 2413,       
-        'furniture_appliances': 0.5, 
-        'clothing': 0.6,             
-        'entertainment': 0.4,        
-        'office_supplies': 0.7,      
-        'personal_care': 0.5,        
-        'services_food': 0.3,        
-        'education': 0.2,            
-        'communication': 0.25,       
-        'loan': 0.15,                
-        'transport': 0.35            
-    }
-}
+from app.constants import EMISSION_FACTORS
 
 class CarbonFootprintCalculator:
     """
@@ -78,21 +24,21 @@ class CarbonFootprintCalculator:
         else:
             # Advanced mode
             public_transit_emissions = (
-                (travel.bus_kms / 1.60934) * EMISSION_FACTORS['public_transit']['bus'] +
-                (travel.transit_rail_kms / 1.60934) * EMISSION_FACTORS['public_transit']['transit_rail'] +
-                (travel.commuter_rail_kms / 1.60934) * EMISSION_FACTORS['public_transit']['commuter_rail'] +
-                (travel.intercity_rail_kms / 1.60934) * EMISSION_FACTORS['public_transit']['intercity_rail']
+                travel.bus_kms * EMISSION_FACTORS['public_transit']['bus'] +
+                travel.transit_rail_kms * EMISSION_FACTORS['public_transit']['transit_rail'] +
+                travel.commuter_rail_kms * EMISSION_FACTORS['public_transit']['commuter_rail'] +
+                travel.intercity_rail_kms * EMISSION_FACTORS['public_transit']['intercity_rail']
             )
             emissions['public_transit'] = public_transit_emissions / 1000
 
             # Air travel: use km directly
-            air_travel_emissionss = (
+            air_travel_emissions = (
                 travel.short_flights * 400 * EMISSION_FACTORS['air_travel']['short'] +
                 travel.medium_flights * 1000 * EMISSION_FACTORS['air_travel']['medium'] +
                 travel.long_flights * 2250 * EMISSION_FACTORS['air_travel']['long'] +
                 travel.extended_flights * 4000 * EMISSION_FACTORS['air_travel']['extended']
             )
-            emissions['air_travel'] = air_travel_emissionss / 1000
+            emissions['air_travel'] = air_travel_emissions / 1000
 
         self.emission.public_transit_emissions = emissions['public_transit']
         self.emission.air_travel_emissions = emissions['air_travel']
